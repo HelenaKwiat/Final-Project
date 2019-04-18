af811aa0fd032c9eade1643082ed2b7b06668954
helena kwiat <hekw7055@colorado.edu>
2019-04-18 12:36:22 -0600

    flask app

--- Sabermetrics/mysite/metric/views.py
+++ Sabermetrics/mysite/metric/views.py
@@ -1,8 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+# def index(request):
+# 	# template = loader.get_template('metric/index.html')
+# 	context = {}
+# 	# return HttpResponse(template.render(context, request))
+# 	return render(request, 'metric/index.html', context)
+
+
+
 def index(request):
-	# template = loader.get_template('metric/index.html')
-	context = {}
-	# return HttpResponse(template.render(context, request))
+    # self.model.objects.all().update(is_immortal=True)
+	context = {'hello': 10}
 	return render(request, 'metric/index.html', context)
+
+
+def details(request):
+	return HttpResponse("You're voting on question ")
+# def set_mortal(request):
+#     # self.model.objects.all().update(is_immortal=False)
+#     self.message_user(request, "All heroes are now mortal")
+#     return HttpResponseRedirect("../")
