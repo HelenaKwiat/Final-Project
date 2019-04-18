f103af105bbbe29a5a2b868e837fcb97ca412820
helena kwiat <hekw7055@colorado.edu>
2019-04-18 12:41:23 -0600

    demo code

--- Sabermetrics/mysite/metric/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MetricConfig(AppConfig):
-    name = 'metric'
--- hello.py
+++ hello.py
@@ -1,9 +1,10 @@
 from flask import Flask
-from flask import render_template
-
 app = Flask(__name__)
 
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
+@app.route('/')
+def hello_world():
+    return 'Home page'
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
